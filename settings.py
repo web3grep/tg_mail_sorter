83a233740ffd3af8eb909c0a8bd517eb288ad2e0
web3grep <122474751+web3grep@users.noreply.github.com>
2023-10-17 22:29:21 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # Введите ваш токен от BotFather
-TOKEN = 'TG_BOT_TOKEN'
+TOKEN = 'TELEGRAM_BOT_TOKEN'
 
 # ID супергруппы, которую нужно сканировать
-SCAN_SUPERGROUP_ID = '-100XXXXXXXXXX'  # Замените на ID вашей супергруппы
+SCAN_CHANNEL_ID = '-100XXXXXXXXXX'  # Замените на ID вашей супергруппы
 
 # ID супергруппы или группы для уведомлений
 NOTIFICATION_CHAT_ID = '-100YYYYYYYYYY'  # Замените на ID вашей супергруппы для уведомлений
