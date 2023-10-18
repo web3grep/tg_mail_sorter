a4b1e31c0b8d8246bba312d59c7fb3556a4161ef
web3grep <122474751+web3grep@users.noreply.github.com>
2023-10-17 18:00:55 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # Введите ваш токен от BotFather
-TOKEN = '6545881821:AAGSQ9EpdEIR5UR_KOpCDsAtikNdlPhmixI'
+TOKEN = 'TG_BOT_TOKEN'
 
 # ID супергруппы, которую нужно сканировать
-SCAN_SUPERGROUP_ID = '-1001916583408'  # Замените на ID вашей супергруппы
+SCAN_SUPERGROUP_ID = '-100XXXXXXXXXX'  # Замените на ID вашей супергруппы
 
 # ID супергруппы или группы для уведомлений
-NOTIFICATION_CHAT_ID = '-1001875110218'  # Замените на ID вашей супергруппы для уведомлений
+NOTIFICATION_CHAT_ID = '-100YYYYYYYYYY'  # Замените на ID вашей супергруппы для уведомлений
