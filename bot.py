484f3c4cc0c916e53f83c9c9d1f8953fb38b7ab8
web3grep <122474751+web3grep@users.noreply.github.com>
2023-10-17 22:27:40 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,41 @@
-from telegram import Bot, Update, ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Chat
 import logging
-import settings
+from settings import TOKEN, SCAN_CHANNEL_ID, NOTIFICATION_CHAT_ID
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-KEYWORDS = set()
+keywords = set()
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update, context):
     update.message.reply_text("Бот запущен. Добавьте ключевые слова с помощью команды /add_keyword.")
-    logger.info(f"Command /start received from {update.message.from_user.username}")
 
-def add_keyword(update: Update, context: CallbackContext) -> None:
-    keyword = update.message.text.split(' ', 1)[1]
-    KEYWORDS.add(keyword)
+def add_keyword(update, context):
+    keyword = update.message.text.split()[-1]
+    keywords.add(keyword)
     update.message.reply_text(f"Ключевое слово '{keyword}' добавлено.")
-    logger.info(f"Keyword '{keyword}' added by {update.message.from_user.username}")
 
-def handle_message(update: Update, context: CallbackContext) -> None:
-    message_text = update.message.text
-    for keyword in KEYWORDS:
-        if keyword in message_text:
-            context.bot.send_message(chat_id=settings.NOTIFICATION_CHAT_ID, text=f"New mail from {keyword}\n[GO](https://t.me/c/{str(update.message.chat.id)[4:]}/{update.message.message_id})", parse_mode=ParseMode.MARKDOWN)
-            logger.info(f"Keyword '{keyword}' found in message: {message_text}")
-
-def error_callback(update: Update, context: CallbackContext) -> None:
-    logger.error(f"Update {update} caused error {context.error}")
+def handle_message(update, context):
+    if update.channel_post:
+        message = update.channel_post
+        for keyword in keywords:
+            if keyword in message.text:
+                context.bot.send_message(chat_id=NOTIFICATION_CHAT_ID, text=f"New mail: {keyword}\n {message.link}")
 
 def main():
-    bot = Bot(token=settings.TOKEN)
-    updater = Updater(token=settings.TOKEN, use_context=True)
-
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("add_keyword", add_keyword))
-    dp.add_handler(MessageHandler(Filters.text & Filters.group, handle_message))
-    dp.add_error_handler(error_callback)
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
